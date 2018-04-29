23c36cbdcc16d91d3ede23fbfe646f7c519bba7a
Sam Vitello <sam.j.vitello@gmail.com>
2018-04-28 22:18:55 -0700

    see how this goes

---
+++ filter_addresses.py
@@ -0,0 +1,16 @@
+import csv
+
+if __name__ == '__main__':
+    seen_addresses = {}
+
+    # strip out duplicates
+    with open('./result.csv') as f:
+        for line in f:
+            line = line.strip()
+            if line == '':
+                break
+            if not seen_addresses.get(line):
+                seen_addresses[line] = True
+
+    for address in seen_addresses.keys():
+        print(address)
